b9d13e96530d4b8a5e34f7eec4ff15ec7d101322
Deryck Hodge <deryckh@gmail.com>
2024-01-09 13:50:06 -0800

    Updates to configs based on new setup.
    I've started using Hyper terminal and bash
    more for personal projects, so need some changes
    to support that.

--- vim/setup.sh
+++ vim/setup.sh
@@ -9,10 +9,13 @@ cp vimrc $HOME/.vimrc
 mkdir -p $VIMCOLORS
 wget https://raw.githubusercontent.com/jonathanfilip/vim-lucius/master/colors/lucius.vim \
     -O $VIMCOLORS/lucius.vim
+cp hyperhacking.vim $VIMCOLORS
 
 git clone https://github.com/VundleVim/Vundle.vim.git $VIMBUNDLE/Vundle.vim
-
 vim +PluginInstall +qall
+
+# Dependencies for YouCompleteMe plugin.
+brew install cmake
 
 # This assumes all the initial setup for Node, Go, etc.
 # has been done. Also requires build-essential and the like
