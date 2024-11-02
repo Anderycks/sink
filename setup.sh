98ce7d8b409089faea709636296e927b65ffa075
Deryck Hodge <deryckh@gmail.com>
2024-11-02 11:29:31 -0400

    Bring back a cross-platform setup script.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,38 @@
 # #!/usr/bin/env bash
 
-sudo apt update
-sudo apt -y install python3 python3-dev nodejs npm vim wget build-essential cmake git
+##
+# Initial environment setup.
+##
+
+# homebrew cd's when it installs, so keep track of
+# where we start to be able to get back. This is
+# safe to do on Linux as well. No harm.
+STARTING_DIR=$(pwd)
 
 ##
-# gnome terminal theme
+# Install depenendencies based on platform.
 ##
-curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.3.0/install.py | python3 -
+MACHINE_NAME="$(uname)"
+if [ $MACHINE_NAME == "Darwin" ]; then
+    eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install bash node python3 vim wget cmake git bash-completion@2
+elif [ $MACHINE_NAME == "Linux" ]; then
+    # Assumes Ubuntu.
+    sudo apt update
+    sudo apt -y install python3 python3-dev nodejs npm vim wget build-essential cmake git
+fi
+
+cd $STARTING_DIR
 
 ##
 # vim
 ##
-VIMBUNDLE=$HOME/.vim/bundle
+VIMHOME=$HOME/.vim
+VIMCOLORS=$VIMHOME/colors
+VIMBUNDLE=$VIMHOME/bundle
 cp vim/vimrc $HOME/.vimrc
+mkdir -p $VIMCOLORS
+cp vim/hyperhacking.vim $VIMCOLORS
 git clone https://github.com/VundleVim/Vundle.vim.git $VIMBUNDLE/Vundle.vim
 vim +PluginInstall +qall
 cd $VIMBUNDLE/YouCompleteMe
